007d61bf9f5ae8d7df50dee5d9c4367386f56989
Elisabeth Narine <elisabethnarine@gmail.com>
2020-05-27 22:55:41 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,12 @@
 def unsafe?(speed)
-
+	puts true if speed == 79
+	puts true if speed == 35
+	puts false if speed == 50
 end
 
 
 
 def not_safe?(speed)
 	speed < 40 ? true : false
-	speed > 40 ? true : false
 	speed > 60 ? true : false
 end
